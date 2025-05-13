157f142884002cac93bc1e11b7b0f7ece631c1db
Hanss3576 <kogmaw1253@gmail.com>
2025-05-14 01:28:05 +0900

    firebase_ui_oauth_google 패키지 추가 및 Google 로그인 공급자 구성

--- lib/model/appsate.dart
+++ lib/model/appsate.dart
@@ -1,4 +1,5 @@
 import 'package:final_exam/firebase_options.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart'
@@ -7,6 +8,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AppSate extends ChangeNotifier {
   // @constructor
@@ -22,6 +24,13 @@ class AppSate extends ChangeNotifier {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+
+    FirebaseUIAuth.configureProviders([
+      GoogleProvider(
+        clientId:
+            '977177627097-lp7b7lm9vpsddc1d8p7ts9umt9r5hu4p.apps.googleusercontent.com',
+      ),
+    ]);
 
     // monitoring user logged state.
     FirebaseAuth.instance.userChanges().listen((user) {
